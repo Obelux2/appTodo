3e5afabb26d8b510342d926eaa0382a789351649
Francisco Velasquez <francisco.velasquezf@gmail.com>
2022-03-22 20:11:00 -0300

    primer commit

--- docs/main.js
+++ docs/main.js
@@ -17,7 +17,7 @@
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "h1 {\r\n    color: indigo\r\n};\r\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "h1 {\n    color: indigo\n};\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
@@ -492,6 +492,159 @@ var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
 
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "L": () => (/* binding */ todoList)
+});
+
+;// CONCATENATED MODULE: ./src/class/todo.class.js
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+
+var Todo = /*#__PURE__*/function () {
+  function Todo(tarea) {
+    _classCallCheck(this, Todo);
+
+    this.tarea = tarea;
+    this.id = new Date().getTime();
+    this.completado = false;
+    this.creado = new Date();
+  }
+
+  _createClass(Todo, null, [{
+    key: "fromJSON",
+    value: function fromJSON(_ref) {
+      var id = _ref.id,
+          tarea = _ref.tarea,
+          completado = _ref.completado,
+          creeado = _ref.creeado;
+      var tempTodo = new Todo(tarea);
+      tempTodo.id = id;
+      tempTodo.completado = completado;
+      tempTodo.creado;
+      return tempTodo;
+    }
+  }]);
+
+  return Todo;
+}();
+;// CONCATENATED MODULE: ./src/class/todo-list.class.js
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function todo_list_class_classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function todo_list_class_defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function todo_list_class_createClass(Constructor, protoProps, staticProps) { if (protoProps) todo_list_class_defineProperties(Constructor.prototype, protoProps); if (staticProps) todo_list_class_defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+
+
+var TodoList = /*#__PURE__*/function () {
+  function TodoList() {
+    todo_list_class_classCallCheck(this, TodoList);
+
+    this.cargarLocalStorage();
+  }
+
+  todo_list_class_createClass(TodoList, [{
+    key: "nuevoTodo",
+    value: function nuevoTodo(todo) {
+      this.todos.push(todo);
+      this.guardarLocalStorage();
+    }
+  }, {
+    key: "eliminarTodo",
+    value: function eliminarTodo(id) {
+      this.todos = this.todos.filter(function (item) {
+        return item.id != id;
+      });
+    }
+  }, {
+    key: "marcarCompletado",
+    value: function marcarCompletado(id) {
+      var _iterator = _createForOfIteratorHelper(this.todos),
+          _step;
+
+      try {
+        for (_iterator.s(); !(_step = _iterator.n()).done;) {
+          var todo = _step.value;
+
+          if (todo.id == id) {
+            todo.completado = !todo.completado;
+            break;
+          }
+        }
+      } catch (err) {
+        _iterator.e(err);
+      } finally {
+        _iterator.f();
+      }
+
+      this.guardarLocalStorage();
+    }
+  }, {
+    key: "eliminarTodos",
+    value: function eliminarTodos() {}
+  }, {
+    key: "eliminarCompletados",
+    value: function eliminarCompletados() {
+      this.todos = this.todos.filter(function (item) {
+        return item.completado != true;
+      });
+      this.guardarLocalStorage();
+    }
+  }, {
+    key: "guardarLocalStorage",
+    value: function guardarLocalStorage() {
+      localStorage.setItem('todo', JSON.stringify(this.todos));
+    }
+  }, {
+    key: "cargarLocalStorage",
+    value: function cargarLocalStorage() {
+      if (localStorage.getItem('todo')) {
+        this.todos = JSON.parse(localStorage.getItem('todo'));
+      } else {
+        this.todos = [];
+      }
+
+      this.todos = this.todos.map(function (obj) {
+        return Todo.fromJSON(obj);
+      });
+    }
+  }, {
+    key: "contarCompletados",
+    value: function contarCompletados() {
+      var suma = 0;
+
+      var _iterator2 = _createForOfIteratorHelper(this.todos),
+          _step2;
+
+      try {
+        for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
+          var completado = _step2.value;
+
+          if (!completado.completado) {
+            suma++;
+          }
+        }
+      } catch (err) {
+        _iterator2.e(err);
+      } finally {
+        _iterator2.f();
+      }
+
+      return suma;
+    }
+  }]);
+
+  return TodoList;
+}();
 // EXTERNAL MODULE: ./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js
 var injectStylesIntoStyleTag = __webpack_require__(379);
 var injectStylesIntoStyleTag_default = /*#__PURE__*/__webpack_require__.n(injectStylesIntoStyleTag);
@@ -542,22 +695,116 @@ var update = injectStylesIntoStyleTag_default()(componentes/* default */.Z, opti
        /* harmony default export */ const css_componentes = (componentes/* default */.Z && componentes/* default.locals */.Z.locals ? componentes/* default.locals */.Z.locals : undefined);
 
 ;// CONCATENATED MODULE: ./src/js/componentes.js
+function componentes_createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = componentes_unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+
+function componentes_unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return componentes_arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return componentes_arrayLikeToArray(o, minLen); }
+
+function componentes_arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+
+
  // import webpacklogo from '../assets/img/webpack-logo.png'
 
-var saluda = function saluda(nombre) {
-  console.log('Creando etiqueta h1');
-  var h1 = document.createElement('h1');
-  h1.innerText = "Hola, ".concat(nombre, "!!!");
-  document.body.append(h1); // console.log('Creando imagen');
-  // const img = document.createElement('img');
-  // img.src = webpacklogo;
-  // document.body.append(img);
+var divTodoList = document.querySelector('.todo-list');
+var txtInput = document.querySelector('.new-todo');
+var btnBorrarCompletados = document.querySelector('.clear-completed');
+var ulFiltos = document.querySelector('.filters');
+var anchorFiltro = document.querySelectorAll('.filtro');
+var cantidad = document.querySelector('strong');
+var crearTodoHtml = function crearTodoHtml(todo) {
+  var htmTodo = "\n        <li class=\"".concat(todo.completado ? 'completed' : '', "\" data-id=\"").concat(todo.id, "\">\n            <div class=\"view\">\n                <input class=\"toggle\" type=\"checkbox\" ").concat(todo.completado ? 'checked' : '', ">\n                <label>").concat(todo.tarea, "</label>\n                <button class=\"destroy\"></button>\n            </div>\n            <input class=\"edit\" value=\"Create a TodoMVC template\">\n        </li>\n        ");
+  var div = document.createElement('div');
+  div.innerHTML = htmTodo;
+  divTodoList.append(div.firstElementChild);
 };
+txtInput.addEventListener('keyup', function (event) {
+  if (event.keyCode === 13) {
+    var nuevoTodo = new Todo(txtInput.value);
+    todoList.nuevoTodo(nuevoTodo);
+    crearTodoHtml(nuevoTodo);
+    txtInput.value = '';
+  }
+});
+divTodoList.addEventListener('click', function (event) {
+  var nombreElemento = event.target.localName;
+  var liElement = event.target.parentElement.parentElement;
+  var idElement = liElement.getAttribute('data-id');
+
+  if (nombreElemento == 'input') {
+    todoList.marcarCompletado(idElement);
+    liElement.classList.toggle('completed');
+    var suma = todoList.contarCompletados();
+    cantidad.innerText = suma;
+    console.log(cantidad);
+  } else if (nombreElemento == 'button') {
+    todoList.eliminarTodo(idElement);
+    divTodoList.removeChild(liElement);
+  } else {}
+});
+btnBorrarCompletados.addEventListener('click', function (event) {
+  todoList.eliminarCompletados();
+
+  for (var i = divTodoList.children.length - 1; i >= 0; i--) {
+    var elemento = divTodoList.children[i];
+
+    if (elemento.classList.contains('completed')) {
+      divTodoList.removeChild(elemento);
+    }
+  }
+});
+ulFiltos.addEventListener('click', function (event) {
+  //console.log(event.target.text);
+  var filtro = event.target.text;
+
+  if (!filtro) {
+    return;
+  }
+
+  anchorFiltro.forEach(function (elem) {
+    return elem.classList.remove('selected');
+  });
+  event.target.classList.add('selected');
+
+  var _iterator = componentes_createForOfIteratorHelper(divTodoList.children),
+      _step;
+
+  try {
+    for (_iterator.s(); !(_step = _iterator.n()).done;) {
+      var elemento = _step.value;
+      elemento.classList.remove('hidden');
+      var completado = elemento.classList.contains('completed');
+
+      switch (filtro) {
+        case 'Pendientes':
+          if (completado) {
+            elemento.classList.add('hidden');
+          }
+
+          ;
+          break;
+
+        case 'Completados':
+          if (!completado) {
+            elemento.classList.add('hidden');
+          }
+
+      }
+    }
+  } catch (err) {
+    _iterator.e(err);
+  } finally {
+    _iterator.f();
+  }
+});
 ;// CONCATENATED MODULE: ./src/index.js
+// import { Todo } from './class/todo.class.js';
 
 
-var nombre = 'Francisco';
-saluda(nombre);
+
+var todoList = new TodoList();
+todoList.todos.forEach(function (todo) {
+  crearTodoHtml(todo);
+});
 })();
 
 /******/ })()
